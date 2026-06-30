f4a2f891c1000cbd5fb821794e232152a8ab4360
Sinovuyo Volo <sinovuyovolo51@gmail.com>
2026-06-30 17:17:52 +0200

    created a deck for our game, started with an empty using a for loop so that it takes in the cards played or drawn

--- uno.py
+++ uno.py
@@ -1,2 +1,15 @@
 import random
 
+COLORS = ["Red", "Blue", "Green", "Yellow"]
+ACTIONS = ["Reverse", "Skip", "Draw 2"]
+WILD_ACTIONS = ["Wild", "Wild Draw 4"]
+
+def create_deck():
+    deck = [] #an empty deck for each time we reshuffle the cards we draw and play should be put in it
+
+    for color in COLORS:
+        for num in range(10):
+            card =  f"{color} {num}"
+            deck.append(card)
+            if num != 0:
+                deck.append(card)
