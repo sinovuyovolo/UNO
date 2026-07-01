8eeb91d89abb6e649f20c552ace9effc1aac7ac6
Sinovuyo Volo <sinovuyovolo51@gmail.com>
2026-07-01 15:52:57 +0200

    if statements o check cards by their colors and values

--- uno.py
+++ uno.py
@@ -26,3 +26,26 @@ def create_deck():
 
     random.shuffle(deck)
     return deck
+
+def can_play_card(card, top_card, current_color):
+
+     # checks if the card is wild and returns true 
+     if "Wild" in card:
+        return True
+     
+     # if we have a normal card, we split the colour and the number of the card
+     card_color, card_value = card.split(" ", 1)
+
+      # checks the card colour we split with colour being played and if true it returns true 
+     if card_color == current_color:
+        return True
+     
+    # this also checks the card but using the value of the card 
+    # if true returns true or else false 
+
+     top_parts = top_card.split(" ", 1)
+     top_value = top_parts[1] if len(top_parts) > 1 else top_parts[0]
+     if card_value == top_value:
+         return True
+     
+     return False #if not true it should return False
